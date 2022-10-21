37b62fced48bc8761d7ff778e5b4d851a4726621
Karine Vitória <karinevitoria0073@gmail.com>
2022-10-21 12:52:30 -0300

    primeiro commit, projeot finalizado

---
+++ js/script.js
@@ -0,0 +1,119 @@
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+const loadingElement = document.querySelector("#loading");
+const postsContainer = document.querySelector("#posts-container");
+
+const postPage = document.querySelector("#post");
+
+// get id from url
+const urlSearchParams = new URLSearchParams(window.location.search);
+const postId = urlSearchParams.get("id");
+const postContainer = document.querySelector("#post-container");
+const commentsContainer = document.querySelector("#comments-container");
+
+const commentForm = document.querySelector("#comment-form");
+const emailInput = document.querySelector("#email");
+const commentInput = document.querySelector("#body");
+
+// get all posts
+async function getAllPosts() {
+    const response = await fetch(url);
+    const data = await response.json();
+    loadingElement.classList.add("hide");
+
+    data.map((post) => {
+        const div = document.createElement("div");
+        const title = document.createElement("h2");
+        const body = document.createElement("p");
+        const link = document.createElement("a");
+
+        title.innerText = post.title;
+        body.innerText = post.body;
+        link.innerText = "Read More";
+        link.setAttribute("href", `post.html?id=${post.id}`);
+
+        div.appendChild(title);
+        div.appendChild(body);
+        div.appendChild(link);
+
+        postsContainer.appendChild(div);
+    });
+}
+
+// get individual post {
+async function getPost(id) {
+    const [responsePost, responseComments] = await Promise.all([
+        fetch(`${url}/${id}`),
+        fetch(`${url}/${id}/comments`),
+    ])
+
+    const dataPost = await responsePost.json();
+
+    const dataComments = await responseComments.json();
+
+    loadingElement.classList.add("hide");
+    postPage.classList.remove("hide");
+
+    const title = document.createElement("h1");
+    const body = document.createElement("p");
+
+    title.innerText = dataPost.title;
+    body.innerText = dataPost.body;
+
+    postContainer.appendChild(title);
+    postContainer.appendChild(body);
+
+    dataComments.map((comment) => {
+        createComment(comment);
+    });
+}
+
+function createComment(comment){
+    const div = document.createElement("div");
+    const email = document.createElement("p");
+    const commentbody = document.createElement("p");
+
+    email.innerText = comment.email;
+    commentbody.innerText = comment.body;
+
+    div.appendChild(email);
+    div.appendChild(commentbody);
+
+    commentsContainer.appendChild(div);
+}
+
+//post a comment
+async function postComment(comment){
+    // POST, PUT PATCH, DELETE - HEADERS TO CONFIGURE
+    const response = await fetch(`${url}/${postId}/comments`, {
+        method: "POST",
+        body: comment,
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+    })
+    
+    const data = await response.json()
+    createComment(data)
+
+}
+
+if (!postId) {
+    getAllPosts();
+} else {
+    getPost(postId);
+
+    //add event to comment form
+    commentForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        let comment = {
+            email: emailInput.value,
+            body: commentInput.value,
+        }
+        
+        comment = JSON.stringify(comment);
+
+        postComment(comment);
+    });
+}
